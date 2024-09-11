fe0c7a9439b90f0cf7245d707f0953bdc943808a
Work <Work@MSI>
2024-09-11 13:17:50 -0500

    Where we ended on September 11

--- ticTacToe-incomplete/ticTacToe.cpp
+++ ticTacToe-incomplete/ticTacToe.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include<string>
+#include <cassert>
 
 using namespace std;
 
@@ -9,12 +10,12 @@ const int ROW_COUNT = 3;
 
 char theGrid[ROW_COUNT][3] =
 {
-    {'a', 'b', 'c' }, //1st row
+    {'a', 'b', 'c'}, //1st row
     {'d', 'e', 'f'},
     {'g', 'h', 'i'}
 };
 
-char currentPlayer = 'X';
+//char currentPlayer = 'X';
 
 void printTheGrid()
 {
@@ -37,9 +38,12 @@ void promptAndUpdateGrid(string playerID)
 
     //cin >> selectedPosition; //ex: 'a', 'd'
 
-    int chosenRow, chosen_column; 
+    int chosenRow, chosen_column;
     //getline()
-    cin >> chosenRow >> chosen_column; 
+    cin >> chosenRow >> chosen_column;
+
+    assert(theGrid[chosenRow][chosen_column] != 'X');
+
 
     if (playerID == "P1")
     {
@@ -56,52 +60,79 @@ void promptAndUpdateGrid(string playerID)
 
 bool checkIfHorizontalWin()
 {
-    bool gameWonHorizontally = false; 
+    //bool gameWonHorizontally = false; 
 
-
-
-    for (int currentColumn = 0; currentColumn < 3; ++currentColumn)
+    for (int currentRow = 0; currentRow < 3; ++currentRow)
     {
-        if (theGrid[0][currentColumn] == currentPlayer)
+        if (theGrid[currentRow][0] == theGrid[currentRow][1]
+            && 
+            theGrid[currentRow][1] == theGrid[currentRow][2])
         {
-            gameWonHorizontally = true; 
+            return true;
         }
-        
-
     }
+
+    return false; 
+
+    
+
+    //gameWonHorizontally = !gameWonHorizontally;
+    //toggle    
+    //Karnaugh maps (don't care) 
+    // 
+    //for (int currentColumn = 0; currentColumn < 3; ++currentColumn)
+    //{
+    //    if (theGrid[0][currentColumn] == currentPlayer)
+    //    {
+    //        gameWonHorizontally = true; 
+    //    }
+    //    
+
+    //}
 }
 
 bool checkIfGameWon()
 {
-    checkIfHorizontalWin(); 
+    return checkIfHorizontalWin();
+
+    //add checks for vertical win 
+    //check for a win on the "main" diagonal (i == j) and the secondary diagonal 
+
 }
 
 int main()
 {
     bool gameIsWon = false; 
 
+    //subject this program to "unit tests"
     while (!gameIsWon)
     {
-        printTheGrid();
+        printTheGrid(); 
 
         string player1 = "P1";
         string player2 = "P2";
-        promptAndUpdateGrid(player1);
 
-        cout << "\033[31m"; //makes the text RED
+        promptAndUpdateGrid(player1);
+        
+        gameIsWon = checkIfGameWon();
+        
+        if (gameIsWon)
+        {
+            cout << "player 1 won\n";
+        }
+        //DRY -> don't repeat yourself 
+
+        //cout << "\033[31m"; //makes the text RED
         printTheGrid(); 
 
         promptAndUpdateGrid(player2);
-
         gameIsWon = checkIfGameWon();
+        if (gameIsWon)
+        {
+            "player TWO won\n";
+        }
 
     }
-
-    
-    //minimalist space approach (can be "unsafe") 
-
-    //vector<vector<char>> dynamicTicTacToeGrid
-
 
     return 0;
 }
